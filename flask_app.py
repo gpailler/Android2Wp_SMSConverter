a00176db8d60e0fd3b4bc56c18d9022925c87d68
Gregoire Pailler <gregoire@pailler.fr>
2016-02-07 22:10:52 +0100

    Added web access to create checksum file

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,16 @@
 from flask import Flask, flash, redirect, url_for, \
                   request, render_template, send_file
-import converter
-import os
-import io
+from os import path, urandom
+from io import BytesIO
 from zipfile import ZipFile
 
+import converter
+
+
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = urandom(24)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-app.debug = True
+app.debug = False
 
 
 @app.route('/')
@@ -26,31 +28,60 @@ def convert():
         flash('Please upload valid Xml and Msg files')
         return redirect(url_for('default'))
 
-    msg_result = io.BytesIO()
-    hsh_result = io.BytesIO()
+    msg_result = BytesIO()
+    hsh_result = BytesIO()
 
     try:
         converter.convert(xml_source.stream, msg_source.stream, msg_result)
+
         msg_result.seek(0)
         hsh_result.write(converter.generate_integrity_hash(msg_result))
+
         msg_result.seek(0)
         hsh_result.seek(0)
-
-        inMemoryOutputFile = io.BytesIO()
-        with ZipFile(inMemoryOutputFile, 'w') as zipFile:
-            zipFile.writestr("result.msg", msg_result.getvalue())
-            zipFile.writestr("result.hsh", hsh_result.getvalue())
-
-        inMemoryOutputFile.seek(0)
-        return send_file(inMemoryOutputFile,
-                         attachment_filename="result.zip",
+        zip_stream = generate_zipfile(msg_result.getvalue(),
+                                      hsh_result.getvalue(),
+                                      'result')
+        return send_file(zip_stream,
+                         attachment_filename='result.zip',
                          as_attachment=True)
-    except:
-        flash('Error during conversion')
+    except Exception as ex:
+        flash(type(ex).__name__ + ' - ' + str(ex.args))
         return redirect(url_for('default'))
-    finally:
-        hsh_result.close()
-        msg_result.close()
 
-if __name__ == "__main__":
+
+@app.route('/createchecksum', methods=['POST'])
+def createchecksum():
+    msg_source = request.files['msgFile']
+
+    if msg_source.filename == '':
+        flash('Please upload valid Msg file')
+        return redirect(url_for('default'))
+
+    hsh_result = BytesIO()
+
+    try:
+        hsh_result.write(converter.generate_integrity_hash(msg_source.stream))
+        hsh_result.seek(0)
+
+        name, _ = path.splitext(msg_source.filename)
+        return send_file(hsh_result,
+                         attachment_filename=name + '.hsh',
+                         as_attachment=True)
+    except Exception as ex:
+        flash(type(ex).__name__ + ' - ' + str(ex.args))
+        return redirect(url_for('default'))
+
+
+def generate_zipfile(msg_content, hsh_content, filename):
+    zip_stream = BytesIO()
+    with ZipFile(zip_stream, 'w') as zip_file:
+        zip_file.writestr(filename + '.msg', msg_content)
+        zip_file.writestr(filename + '.hsh', hsh_content)
+
+    zip_stream.seek(0)
+    return zip_stream
+
+
+if __name__ == '__main__':
     app.run()
